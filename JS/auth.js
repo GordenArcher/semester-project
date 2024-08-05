2c4c3d834eedb1870e504288d12bbfbc462a58c0
School <school@Gordens-MacBook-Pro.local>
2024-08-05 01:00:41 +0000

    starting project

---
+++ JS/auth.js
@@ -0,0 +1,8 @@
+const user_name = document.getElementById('username')
+const pass_word = document.getElementById('password')
+
+
+export function test(name){
+    alert(name)
+}
+
---
+++ JS/index.js
@@ -0,0 +1,33 @@
+import test from './auth.js'
+
+// const nav_links = document.querySelectorAll('.links ul li')
+
+// nav_links.forEach((tabs) => {
+//     tabs.addEventListener('click', () => {
+//         nav_links.forEach((i) => {
+//             i.classList.remove('active')
+//         })
+
+//         tabs.classList.add('active')
+//     })
+
+
+// })
+
+test('IT WORKED')
+// let username_holder = document.querySelector('.username_holder')
+// let password_holder = document.querySelector('.password_holder')
+
+// document.addEventListener('submit', (ee) => {
+//     ee.preventDefault()
+//     let username = user_name.value
+//     let password = pass_word.value
+    
+//     // alert(username + password)
+
+//     username_holder.innerHTML = username
+//     password_holder.innerHTML = password
+
+//     // document.location.href = '/'
+// })
+
