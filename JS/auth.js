78714fcdbba85be3f01a2721c96a5a3b11c4cef7
School <school@Gordens-MacBook-Pro.local>
2024-08-12 02:02:03 +0000

    made some changes

--- JS/auth.js
+++ JS/auth.js
@@ -1,8 +1,17 @@
 const user_name = document.getElementById('username')
 const pass_word = document.getElementById('password')
+const firstname = document.getElementById('firstname')
+const lastname = document.getElementById('firstname')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+const firstnameLable = document.querySelector('.firstname label')
 
-
-export function test(name){
-    alert(name)
-}
-
+firstname.addEventListener('input', () => {
+    if(firstname.value.length > 0){
+        firstnameLable.classList.add('active')
+    }else{
+        firstnameLable.classList.remove('active')
+    }
+})
