2d03a0b8d0de4f59c81000d345d49c7ba91f35e5
GordenArcher <archergorden@gmail.com>
2024-08-23 21:13:34 +0000

    fixed a little bag with the index.js

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,3 @@ const nav = document.querySelector('.nav_links')
 mobile.addEventListener('click', function() {
     nav.classList.toggle('nav_active')
 } )
-
-window.onclick = function() {
-    nav.style.display = 'none'
-}
